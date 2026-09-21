4c7668466645d746d63d3b485fcd4c20c5e5d4aa
Solesi Samuel <samuelsolesi377@gmail.com>
2026-09-21 11:29:25 +0200

    Fix production CORS

--- config/cors.php
+++ config/cors.php
@@ -1,16 +1,30 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'register',
+        'login',
+        'logout',
+        'sanctum/csrf-cookie',
+    ],
+
     'allowed_methods' => ['*'],
+
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://dinehub.vercel.app',
+        'https://dinehub-nu.vercel.app',
     ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
